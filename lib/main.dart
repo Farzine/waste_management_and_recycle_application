a6e3f3e8bbd97e5e21dc28f6184ae49651a9bf8a
Farzine <farzine07@student.sust.edu>
2023-12-29 02:46:07 +0600

    home screen ui done

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:waste_management_and_recycle_application/auth/sign_in.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:waste_management_and_recycle_application/screens/home_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,7 +23,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: SignIn(),
+      home: HomeScreen(),
     );
   }
 }
