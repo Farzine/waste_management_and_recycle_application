81d19da302d82ddcff8a68dc9722c2ecfb840e25
Farzine <farzine07@student.sust.edu>
2023-12-30 00:19:58 +0600

    managed home

---
+++ lib/screens/home/waste_type.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class WasteType extends StatelessWidget {
+  final String WasteTypeImage;
+  final String WasteTypeName;
+  final String WasteTypeSubTitle;
+  final Function onTap;
+  WasteType(
+      {required this.WasteTypeImage,
+      required this.WasteTypeName,
+      required this.WasteTypeSubTitle,
+      required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: [
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 10),
+            height: 200,
+            width: 150,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Color.fromARGB(255, 165, 248, 165),
+            ),
+            child: Column(
+              children: [
+                Expanded(
+                  flex: 3,
+                  child: Image(
+                    image: AssetImage(WasteTypeImage),
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    children: [
+                      Container(
+                        height: 45,
+                        width: 145,
+                        child: OutlinedButton(
+                          style: OutlinedButton.styleFrom(
+                            backgroundColor: Color.fromARGB(255, 86, 161, 71),
+                          ),
+                          onPressed: () {},
+                          child: Column(
+                            children: [
+                              Text(
+                                WasteTypeName,
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                WasteTypeSubTitle,
+                                style: TextStyle(
+                                    color: Color.fromARGB(255, 44, 44, 44)),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
